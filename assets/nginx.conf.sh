d741fd09f2b69c279f156c629b1b83c3c358ee9a
opensourcer2 <opencrypto@protonmail.com>
2020-03-06 18:30:28 -0600

    Update nginx.conf.sh

--- assets/nginx.conf.sh
+++ assets/nginx.conf.sh
@@ -8,6 +8,22 @@ server {
 
   location / {
     autoindex on;
+  }  
+  location /ui/ {
+    try_files $uri $uri/ /ui/index.html;
+    autoindex off;
+  }
+  location /dist/ {
+    autoindex on;
+  }
+  location /pool/ {
+    autoindex on;
+  }
+  location /api/ {
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $remote_addr;
+    proxy_set_header Host $host;
+    proxy_pass http://127.0.0.1:8080;
   }
 }
 EOF
