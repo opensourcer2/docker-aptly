da086e2b6fd1c2f607607727ce08e34d3bca5cb1
opensourcer2 <opencrypto@protonmail.com>
2020-03-07 18:48:22 -0600

    Update nginx.conf.sh

--- assets/nginx.conf.sh
+++ assets/nginx.conf.sh
@@ -4,13 +4,15 @@ cat << EOF > /etc/nginx/conf.d/default.conf
 server_names_hash_bucket_size 64;
 server {
   root /opt/aptly/public;
+  index index.html index.htm;
   server_name ${HOSTNAME};
+  autoindex off;
 
   location / {
     autoindex on;
   }  
   location /ui/ {
-    try_files $uri $uri/ /ui/index.html;
+    try_files \$uri \$uri/ /ui/index.html;
     autoindex off;
   }
   location /dist/ {
@@ -23,10 +25,10 @@ server {
 		proxy_redirect	off;
 		proxy_pass	http://localhost:8080/api/;
 		proxy_redirect	http://localhost:8080/api/ /api;
-		proxy_set_header X-Real-IP $remote_addr;
-		proxy_set_header X-Forwarded-For $remote_addr;
-		proxy_set_header X-Forwarded-Proto $scheme;
-		proxy_set_header Host $http_host;
+		proxy_set_header X-Real-IP \$remote_addr;
+		proxy_set_header X-Forwarded-For \$remote_addr;
+		proxy_set_header X-Forwarded-Proto \$scheme;
+		proxy_set_header Host \$http_host;
 		proxy_set_header Origin "";
   }
 }
